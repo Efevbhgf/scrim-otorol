b78d7e3c65e09efd111a902800bc52710b3309eb
Efevbhgf <efemustafa190777@gmail.com>
2025-04-18 18:55:53 +0300

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,9 +1,9 @@
 module.exports = {
-  prefix: "BOT PREFİX",
-  sahip: "Senin ID'n",
+  prefix: "!",
+  sahip: "1310461903025209414",
   color: 0x0074ff,
-  proseries: "PRO SERİES ROL ID",
-  proserieskanal: "PRO SERİES KANAL ID",
-  emoji: "EMOJİ",
-  token: "BOT TOKEN"
+  proseries: "1362817960204308490",
+  proserieskanal: "1362817907951931712",
+  emoji: "1362818301842952213",
+  token: "MTM2MjgxODU2MTU3NTIyMzM2Ng.G0xX1W.qHip1qGbk246rUaEFw26gML2L8k7yidg6r8VNs"
 }
